1b6e251b6e8eb9c5506138134b4152566c065dd9
scarf <greenscarf005@gmail.com>
2022-10-22 14:05:15 +0900

    refactor: use to_list
    from latest flupy

--- tileset_tools/list_tileset_ids.py
+++ tileset_tools/list_tileset_ids.py
@@ -3,6 +3,7 @@ from msgspec import Struct
 import msgspec
 from pathlib import Path
 
+
 from tileset_tools.model.TileConfig import TileConfigBase
 from tileset_tools.util import lift_list
 
@@ -24,11 +25,10 @@ id_decoder = msgspec.json.Decoder(TileIDs)
 
 def get_tileset_ids(path: Path) -> list[str]:
     tiles_new = id_decoder.decode(path.read_bytes()).tiles_new
-    res: list[str] = (
+    return (
         flu(tiles_new)
         .map(lambda x: x.tiles)
         .map(lambda x: flu(x).map(lambda y: lift_list(y.id)))
         .flatten(depth=2)
-        .collect()  # type: ignore
+        .to_list()
     )
-    return res
